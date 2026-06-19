19d1c9867815e257e60ac523660dd2f7e7a52338
khandakar Jarin Shova <jarinshova007@gmail.com>
2026-06-19 11:27:27 +0600

    initial commit

---
+++ topic_08_binaryTree/SumOfNodes.java
@@ -0,0 +1,59 @@
+package topic_08_binaryTree;
+
+// Build a tree using preorder traversal with null markers and find the sum of nodes
+public class SumOfNodes {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        int idx = -1;
+
+        public Node buildTree(int nodes[]) {
+            idx++;
+
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(h)
+    // where n = number of nodes and h = height of the tree = maximum recursion
+    // stack depth
+    public static int sumOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+
+        return leftSum + rightSum + root.data;
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+
+        // sumOfNodes method call
+        System.out.println("Sum of nodes = " + sumOfNodes(root));
+    }
+}
