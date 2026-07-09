f141eef562d6d47d222f6778d2e9bdd01d351940
khandakar Jarin Shova <jarinshova007@gmail.com>
2026-07-09 21:23:21 +0600

    initial commit

---
+++ topic_09_BST/DeleteANode.java
@@ -0,0 +1,103 @@
+package topic_09_BST;
+
+public class DeleteANode {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            // root = new Node(val);
+            // return root;
+            return new Node(val);
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    // delete
+    public static Node delete(Node root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        // search and delete the key
+        if (root.data > key) {
+            root.left = delete(root.left, key);
+        } else if (root.data < key) {
+            root.right = delete(root.right, key);
+        } else { // root.data == key
+            // case 1 - leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // case 2 - one child node
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // case 3 - two leaf node
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+
+        return root;
+    }
+
+    // finds the leftmost node from the right subtree
+    public static Node inorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    // Inorder traversal of BST gives a sorted sequence
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        // preorder
+        int values[] = { 5, 3, 2, 1, 4, 6 };
+
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inorder(root);
+        System.out.println();
+
+        // delete method call
+        root = delete(root, 5);
+
+        inorder(root);
+    }
+}
