09fa4b0efee876798a74468c2739e015c8356dc8
khandakar Jarin Shova <jarinshova007@gmail.com>
2026-08-03 23:35:19 +0600

    initial commit

---
+++ topic_02_advanced_sorting/CountSort.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class CountSort {
+    // Overall Time Complexity: O(n + k)
+    // Overall Space Complexity: O(n + k)
+    public static void countSort(int arr[]) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        // create a new array
+        int count[] = new int[max + 1];
+
+        // Initialize all indices to 0 (it's optional for java as new int[] sets 0 to
+        // every index automatically)
+        // for (int i = 0; i < (max + 1); i++) {
+        // count[i] = 0;
+        // }
+
+        // frequency array of count
+        for (int i = 0; i < arr.length; i++) {
+            // count[arr[i]] = count[arr[i]] + 1;
+            count[arr[i]]++;
+        }
+
+        // cumulative frequency array (prefix sum) of count
+        for (int i = 1; i < count.length; i++) {
+            // count[i] = count[i] + count[i - 1];
+            count[i] += count[i - 1];
+        }
+
+        // create a new array of size (arr.length)
+        int result[] = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            result[--count[arr[i]]] = arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = result[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("size = ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // method call
+        countSort(arr);
+
+        // print array
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+
+    }
+}
